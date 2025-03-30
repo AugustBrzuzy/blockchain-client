5e806a30c93857e0819e1dc4ea89e561cfd53911
August Brzuzy <augustbrzuzy@gmail.com>
2025-03-30 14:07:11 +0200

    wording

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,3 +1,7 @@
+## assumption is that there is a pre-existing central ECS cluster
+## adding ECS task definition and ECS service to the existing cluster per the requirement on given app to be deployed
+## also expecting a separate infra layer for networking and security group creation that precedes this module
+
 data "aws_subnet" "ecs_subnet" {
   id = var.subnet_id
 }
